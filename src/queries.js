49860c27565d39110b8ac62011d41b2e411cdcc3
Kevin Davis <Kevindavis5454@gmail.com>
2020-07-27 14:26:04 -0600

    Added Auth

--- src/queries.js
+++ src/queries.js
@@ -17,6 +17,7 @@ const userAuth = (request, response) => {
         if (results.rows[0].password == password) {
             response.cookie('user_id', results.rows[0].user_id, {
                 /*signed: true,*/
+                domain: 'cutthroat-losers.vercel.app',
                 httpOnly: false,
                 sameSite: 'none',
                 secure: false,
