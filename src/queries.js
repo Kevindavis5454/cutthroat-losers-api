1d6dcd07a1fd14c05fe81e2d98233727b345835d
Kevin Davis <Kevindavis5454@gmail.com>
2020-07-28 13:23:43 -0600

    Added Auth

--- src/queries.js
+++ src/queries.js
@@ -16,7 +16,7 @@ const userAuth = (request, response) => {
         }
         if (results.rows[0].password == password) {
             response.cookie('user_id', results.rows[0].user_id, {
-                domain: '*.herokuapp.com',
+                domain: 'herokuapp.com',
                 httpOnly: false,
                 signed: false,
                 sameSite: 'none',
