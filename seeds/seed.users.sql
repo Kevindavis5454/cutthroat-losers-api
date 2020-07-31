7f885f39132d45c8145f890e7902caba5be59825
Catherine Davis <catherinevtdavis@gmail.com>
2020-07-31 13:26:54 -0600

    added sql files

---
+++ seeds/seed.users.sql
@@ -0,0 +1,19 @@
+INSERT INTO public.users
+("password", display_name, date_created, username)
+VALUES
+    ('test123', 'Todd', now(), 'todd@gmail.com'),
+    ('test123', 'Ben', now(), 'ben@gmail.com'),
+    ('test123', 'Sarah', now(), 'sarah@gmail.com'),
+    ('test123', 'Jeanne', now(), 'jeanne@gmail.com'),
+    ('test123', 'Laura', now(), 'laura@gmail.com'),
+    ('test123', 'Angel', now(), 'angel@gmail.com'),
+    ('test123', 'Emma', now(), 'emma@gmail.com'),
+    ('test123', 'James', now(), 'james@gmail.com'),
+    ('test123', 'Ralph', now(), 'ralph@gmail.com'),
+    ('test123', 'Matt', now(), 'matt@gmail.com'),
+    ('test123', 'Jim', now(), 'jim@gmail.com'),
+    ('test123', 'Lucy', now(), 'lucy@gmail.com'),
+    ('test123', 'Katy', now(), 'katy@gmail.com'),
+    ('test123', 'Kevin', now(), 'kevin@gmail.com'),
+    ('test123', 'Galen', now(), 'galen@gmail.com'),
+    ('test123', 'Kristina', now(), 'kristina@gmail.com');
