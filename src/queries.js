59a11e829ce2af1e9400c2d36a8aa4ecf85fc550
Kevin Davis <Kevindavis5454@gmail.com>
2020-08-02 15:18:24 -0600

    Working on Contest Data pull

--- src/queries.js
+++ src/queries.js
@@ -215,7 +215,7 @@ const contestSabotages = (contest_id, cb) => {
 }
 
 const contestUsers = (contest_id, cb) => {
-    pool.query('SELECT user_id FROM contest_to_user WHERE contest_id = $1 ORDER BY user_id ASC', [contest_id], (error, results) => {
+    pool.query('SELECT * FROM contest_to_user WHERE contest_id = $1 ORDER BY user_id ASC', [contest_id], (error, results) => {
         if (error) {
             throw error
         }
