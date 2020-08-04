c084b497746a59c6ba14a91e2364dc6e925f4936
Catherine Davis <catherinevtdavis@gmail.com>
2020-08-04 00:43:19 -0600

    added more seeding pages

---
+++ seeds/seed.win.sql
@@ -0,0 +1,8 @@
+INSERT INTO public.win
+(win, contest_id, date_created)
+VALUES
+    ('weight', 4, now()),
+    ('stomach', 4, now()),
+    ('workout', 4, now()),
+    ('bingo', 4, now()),
+    ('admin', 4, now());
