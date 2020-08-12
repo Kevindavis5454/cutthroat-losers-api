22daacc06a1b09d5323354738368de470d34e82d
Kevin Davis <Kevindavis5454@gmail.com>
2020-08-12 09:51:18 -0600

    Refactoring Routes

--- src/queries.js
+++ src/queries.js
@@ -361,7 +361,7 @@ const getUserPoints = (user_id, contest_id, cb) => {
 }
 
 const getPointsGained = (user_id, contest_id, cb) => {
-    pool.query("SELECT SUM(points) FROM points WHERE category='stomach' OR category='weight' OR category='workout' AND user_id = $1 AND contest_id = $2", [user_id, contest_id], (error, results) => {
+    pool.query("SELECT points FROM points WHERE category='stomach' OR category='weight' OR category='workout' AND user_id = $1 AND contest_id = $2", [user_id, contest_id], (error, results) => {
         if (error) {
             throw error
         }
