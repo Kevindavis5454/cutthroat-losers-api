bd1f872e5cf1129909019c021e89f29da8aa7d32
Catherine Davis <catherinevtdavis@gmail.com>
2020-08-12 01:10:31 -0600

    removed sort from query

--- src/queries.js
+++ src/queries.js
@@ -260,7 +260,7 @@ const bingoPointsValue = (user_id, cb) => {
 }
 
 const weightPointsValue = (user_id, cb) => {
-    pool.query("SELECT SUM(points) FROM points WHERE category = 'weight' AND user_id = $1 ORDER BY user_id ASC", [user_id], (error, results) => {
+    pool.query("SELECT SUM(points) FROM points WHERE category = 'weight' AND user_id = $1", [user_id], (error, results) => {
         if (error) {
             throw error
         }
