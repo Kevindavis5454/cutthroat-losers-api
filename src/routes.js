0867d71127187db2721ef223eb3f3c1d3275f7fa
Catherine Davis <catherinevtdavis@gmail.com>
2020-08-21 14:36:00 -0600

    error handling

--- src/routes.js
+++ src/routes.js
@@ -15,8 +15,8 @@ const createUser = (request, response, next) => {
          db.createUser(display_name, username, password, function(results){
              console.log(results, "results from db.createUser")
              try{
-                 if (!results.rows[0].user_id){
-                     throw new Error
+                 if (results === 23505){
+                     response.status(401).send("Email already in use")
                  } else {
                     response.status(201).send(`User added with USER ID: ${results.rows[0].user_id}`)
                  }
@@ -24,7 +24,6 @@ const createUser = (request, response, next) => {
              }
              catch(err){
                 console.log(err)
-                response.status(401).send("Email already in use")
              }
          })      
      }
