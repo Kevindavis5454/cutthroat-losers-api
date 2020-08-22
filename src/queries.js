c07e5ca3e329542aef937eeb0f4945a21b4cc66a
Kevin Davis <Kevindavis5454@gmail.com>
2020-08-22 15:37:09 -0600

    Worked on signup auth

--- src/queries.js
+++ src/queries.js
@@ -71,7 +71,7 @@ const getUserByUsername = (username, cb) => {
 }
 
 const checkUserByUsername = (username, cb) => {
-    pool.query('SELECT username FROM users WHERE username = $1', [username], (results) => {
+    pool.query('SELECT * FROM users WHERE username = $1', [username], (results) => {
         cb(results)
     })
 }
